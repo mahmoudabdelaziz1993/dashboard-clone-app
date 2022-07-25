17790c744d01e7259e6c26f6955fa6ef433ea81e
mahmoudabdelaziz1993 <mahmoudabdelaziz1993@outlook.com>
2022-07-25 19:13:08 +0200

    Feat: Dummy Cool navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { Nav } from "./components/Nav";
+
 function App() {
   return (
     <div className="w-full h-[100vh] grid md:grid-cols-12 grid-cols-1">
-      <div className="col-span-1 p-3 md:col-span-3">1</div>
+      <div className="col-span-1 p-3 md:col-span-3">
+        <Nav />
+      </div>
       <div className="col-span-1 pt-3 pl-3 md:col-span-9">2</div>
       {/* <h2 className="text-5xl text-red-300"> hay dude !</h2> */}
     </div>
---
+++ src/components/Nav.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { FaSlackHash } from "react-icons/fa";
+export const Nav = () => {
+  return (
+    <div className="flex gap-8 md:flex-col ">
+      <div className="flex items-center gap-2">
+        <FaSlackHash className="text-4xl" />
+        <h3 className="text-lg font-bold uppercase">Logo</h3>
+      </div>
+      <ul className="w-56 p-2 menu md:menu-vertical menu-horizontal bg-base-100 rounded-box">
+        <li className="bordered">
+          <a href="!#">
+            <FaSlackHash />
+            <span className="hidden md:flex"> Item 1</span>
+          </a>
+        </li>
+        <li>
+          <a href="!#">
+            <FaSlackHash />
+            <span className="hidden md:flex"> Item 2</span>
+          </a>
+        </li>
+        <li tabindex="0">
+          <span>
+            {" "}
+            <FaSlackHash />
+            <span className="hidden md:flex"> Item 3</span>
+          </span>
+          <ul className="bg-base-300">
+            <li>
+              <a href="!#">
+                {" "}
+                <FaSlackHash />
+                Submenu 1
+              </a>
+            </li>
+            <li>
+              <a href="!#">Submenu 2</a>
+            </li>
+            <li>
+              <a href="!#">Submenu 3</a>
+            </li>
+          </ul>
+        </li>
+      </ul>{" "}
+    </div>
+  );
+};
